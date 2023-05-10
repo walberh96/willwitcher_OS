6f13cee2b278213595a88e85e3c3b95f5cfea7ad
Willwitcher <walberh96@gmail.com>
2023-05-10 13:19:58 -0400

    later_install script added

---
+++ install_later.sh
@@ -0,0 +1,19 @@
+#INSTALL YAY AUR HELPER
+cd .gitRepos/yay/
+makepkg -si
+
+#INSTALL YAY PACKAGES
+
+yay -S gpu-passthrough-manager 
+looking-glass
+freetube
+timeshift
+vscodium
+betterlockscreen
+envycontrol
+sddm-theme-catppuccin-git
+nemo-fileroller-git
+nemo-terminal-git
+noto-color-emoji-fontconfig
+nb
+
